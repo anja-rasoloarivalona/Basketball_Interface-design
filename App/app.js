d6b3b9af4bd53b6d6e77c0cd88a3e543d087c5df
Anja <rasoloanja@gmail.com>
2019-03-18 21:07:12 -0400

    Building the score-board

--- App/app.js
+++ App/app.js
@@ -8,5 +8,21 @@ $(document).ready(function(){
 
 
         });
+
+        $(window).scroll(function() {
+            if($(document).scrollTop() > 10) {
+                 $('.header').addClass('header--scroll');
+                 $('.header__sidebar__btn__hamburger').addClass('header--scroll__btn');
+                 $('.header__icon').addClass('header--scroll__icon');
+                 $('.header__items').addClass('header--scroll__items');
+            }
+            else {
+                $('.header').removeClass('header--scroll');
+                $('.header__sidebar__btn__hamburger').removeClass('header--scroll__btn');
+                $('.header__icon').removeClass('header--scroll__icon');
+                $('.header__items').removeClass('header--scroll__items');
+            }
+        });
 });
+
 
